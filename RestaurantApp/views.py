16854f9755d93a2beb73b255e38276a6f204db7c
Amin <aminmalek5@gmail.com>
2021-03-08 15:24:58 +0330

    new FBV added

--- RestaurantApp/views.py
+++ RestaurantApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import ReserveTable
 from .models import Food
+from .models import About
+from .models import ContactForm
+from django.core.mail import send_mail
+
 def index (request):
     return render(request, 'RestaurantApp/index.html',{})
 
@@ -24,5 +28,44 @@ def MenuView(request):
 	}
 
 	return render(request,'RestaurantApp/menu.html',context)
+
+def ContactView(request):
+	if request.method == "POST":
+		name = request.POST['name']
+		email = request.POST['email']
+		subject = request.POST['subject']
+		message = request.POST['message']
+		inst = ContactForm(name=name,email=email,subject=subject,message=message)
+		inst.save()
+
+		"""send_mail(
+
+			name,
+			email,
+			subject,
+			message,
+			['aminmalek5@gmail.com']
+
+			)"""
+		#sending email
+		return render(request,'RestaurantApp/contact.html',{'name' : name})
+	else:
+		return render(request,'RestaurantApp/contact.html')
+
+
+def AboutView(request):
+	query = About.objects.all()
+	context ={
+	'object_list':query
+
+	}
+	return render(request,'RestaurantApp/about.html',context)
+
+
+
+def GalleryView(request):
+
+	return render(request,'RestaurantApp/gallery.html')
+
 
 
